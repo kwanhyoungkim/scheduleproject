33cb6e9197558924119d0c4ebe1be6cefc98447f
t2024-m0199 <kimgh961021@gmail.com>
2025-05-01 20:01:52 +0900

    Initial commit

---
+++ src/main/java/com/example/scheduleproject/ScheduleprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scheduleproject/ScheduleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
